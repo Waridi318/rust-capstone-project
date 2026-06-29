4955c0cfc5a5bd6a724e6e68807838d70757ca48
Waridi318 <rosenjeri7151@gmail.com>
2026-06-29 18:52:50 +0300

    implemented core Bitcoin RPC workflow with wallet creation, mining, transaction sending, and block confirmation

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -17,7 +17,7 @@ const RPC_PASS: &str = "password";
 // You can also use serde_json `Deserialize` derivation to capture the returned json result.
 fn send(rpc: &Client, addr: &str) -> bitcoincore_rpc::Result<String> {
     let args = [
-        json!([{addr : 100 }]), // recipient address
+        json!([{addr : 20 }]), // recipient address
         json!(null),            // conf target
         json!(null),            // estimate mode
         json!(null),            // fee rate in sats/vb
@@ -47,14 +47,15 @@ fn main() -> bitcoincore_rpc::Result<()> {
 
     // Create/Load the wallets, named 'Miner' and 'Trader'.
     //Create wallet "Miner" and load it if it exists
-    match rpc.create_wallet ("Miner", None, None, None, None, None, None ) {
+    match rpc.create_wallet ("Miner", None, None, None, None) {
         Ok(_) => (),
         Err(_) => {
             rpc.load_wallet("Miner")?;
         }
     }
 
-     match rpc.create_wallet ("Trader", None, None, None, None, None, None) {
+    //Create Wallet "Trader" and load it if ot exists
+    match rpc.create_wallet ("Trader", None, None, None, None) {
         Ok(_) => (),
         Err(_) => {
             rpc.load_wallet("Trader")?;
@@ -66,23 +67,38 @@ fn main() -> bitcoincore_rpc::Result<()> {
     let miner_address = rpc.get_new_address (Some("Mining reward"), None)?;
     let address = miner_address.assume_checked();
 
-    // 101 blocks need to be mined to this address
+    //101 blocks need to be mined to this address
+    //1 block for reward + 100 blocks for confirmations
     rpc.generate_to_address(101, &address)?;
 
     //check balance
     let balance = rpc.get_balance(None)?;
 
     // Load Trader wallet and generate a new address
+    let trader_address = rpc.get_new_address(Some("Received"), None)?;
+    let trader_addr_str = trader_address.to_string();
 
     // Send 20 BTC from Miner to Trader
+    let txid = send(&rpc, &trader_addr_str)?;
 
     // Check transaction in mempool
+    let mempool = rpc.get_raw_mempool()?;
 
     // Mine 1 block to confirm the transaction
+    rpc.generate_to_address(1, &address)?;
 
+    //Get transaction details and block hash
+
+    let tx_info = rpc.get_transaction(&txid, None)?;
+    let block_height = tx_info.block_height.unwrap_or(0);
+    let block_hash = rpc.get_block_hash(block_height)?;
+    
     // Extract all required transaction details
 
     // Write the data to ../out.txt in the specified format given in readme.md
-
+    let mut file = File::create("../out.txt")?;
+    writeln!(file, "{}", txid)?;
+    writeln!(file, "{}", block_height)?;
+    writeln!(file, "{}", block_hash)?;
     Ok(())
 }
